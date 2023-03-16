9cd132ddfbb88a62f17385391bb955124ab4eb09
AndreySozinov <savrey@ya.ru>
2023-03-16 22:15:29 +0500

    All three tasks solved

--- Homework01.py
+++ Homework01.py
@@ -2,3 +2,14 @@
 # Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
 
+origin_list = [5, 2, 6, 5, 9, 1, 6, 6, 6]
+doubles_list = []
+
+for item in origin_list:
+    if origin_list.count(item) > 1 and item not in doubles_list:
+        doubles_list.append(item)
+
+result_list = set(origin_list)
+
+print(doubles_list)
+print(result_list)
--- Homework03.py
+++ Homework03.py
@@ -3,3 +3,15 @@
 # Достаточно вернуть один допустимый вариант.
 # *Верните все возможные варианты комплектации рюкзака.
 
+BACKPACK_CAPACITY = 20
+things_dict = {'rope': 2, 'pot': 5, 'matches': 1, 'axe': 10, 'notebook': 5, 'pizza': 3}
+
+total_weight = 0
+for item, weight in things_dict.items():
+    total_weight += things_dict[item]
+    if total_weight <= BACKPACK_CAPACITY:
+        print(item, weight)
+    else:
+        break
+
+
